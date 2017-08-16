4d2ffad63df7260602b8609a656e0a1e2ee350da
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2017-08-16 09:42:02 +0200

    Update prelude.js

--- prelude.js
+++ prelude.js
@@ -1,11 +1,10 @@
 // A function for Funk Function Application (required to support methods on primitive types in JavaScript)
 function _A(f, x) {
-    if(typeof f == 'function') {
-        if(x === true) x = 'True';
-        if(x === false) x = 'False';
+    if(typeof f === 'function') {
         return f(x);
-    } else if(typeof f == 'string') {
+    } else if(typeof f === 'string') {
         switch(x) {
+            case 'Show': return JSON.stringify(x);
             case 'Size': return f.length;
             case 'CharAt': return function(i) { return f.charAt(i); };
             case 'CharCodeAt': return function(i) { return f.charCodeAt(i); };
@@ -17,10 +16,12 @@ function _A(f, x) {
             case '==': return function(v) { return f == v; };
             case '!=': return function(v) { return f != v; };
             case '+': return function(v) { return f + v; };
+            case '!_': return function(v) { return (v === true || v === 'True') ? 'False' : 'True'; };
             default: throw "Unexpected argument: " + x;
         }
-    } else if(typeof f == 'number') {
+    } else if(typeof f === 'number') {
         switch(x) {
+            case 'Show': return "" + x;
             case '==': return function(v) { return f == v; };
             case '!=': return function(v) { return f != v; };
             case '>': return function(v) { return f > v; };
@@ -35,6 +36,12 @@ function _A(f, x) {
             case '/': return function(v) { return f / v; };
             default: throw "Unexpected argument: " + x;
         }
+    } else if(f === true) {
+        return _A('True', x);
+    } else if(f === false) {
+        return _A('False', x);
+    } else if(f == null) {
+        return _A('Unit', x);
     } else {
         throw "Unexpected argument: " + x;
     }
@@ -47,5 +54,19 @@ var system_ = function(x) {
         case 'Dir': return function(v) { console.dir(v) };
         case 'SetText': return function(v) { document.getElementById("result").textContent = v; };
         case 'SetHtml': return function(v) { document.getElementById("result").innerHTML = v; };
+    }
+}
+
+// Mutable "variables" as a library function. Read with unit application, eg. "foo()".
+var new_ = function(x) {
+    return function(t) {
+        if(t == null) return x;
+        switch(t) {
+            case 'Unit': return x;
+            case '=': return function(y) { x = y; };
+            case '+=': return function(y) { x += y; };
+            case '-=': return function(y) { x -= y; };
+            case '*=': return function(y) { x *= y; };
+        }
     }
 }
