1636f73c59f259087922fb3de5b46fa9c800ca71
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2017-08-24 18:49:12 +0200

    Update prelude.js

--- prelude.js
+++ prelude.js
@@ -39,13 +39,21 @@ function _A(f, x) {
     } else if(Array.isArray(f)) {
         switch(x) {
             case 'Show': return "[" + f.join(", ") + "]";
-            case 'At': return function(v) { return f[v]; };
-            case 'Get': return function(v) { return v < 0 || v >= f.length ? function(g) { return _A(g, 'None'); } : function(g) { return _A(_A(g, 'Some'), f[v]); }};
+            case 'Get': return function(v) { return _O(f[v]); };
             case 'Size': return f.length;
-            case 'Head': return f.length == 0 ? function(g) { return _A(g, 'None'); } : function(g) { return _A(_A(g, 'Some'), f[0]); };
-            case 'Tail': return f.length == 0 ? function(g) { return _A(g, 'None'); } : function(g) { return _A(_A(g, 'Some'), f.slice(1)); };
+            case 'Head': return _O(f[0]);
+            case 'Tail': return _O(f.length == 0 ? void 0 : f.slice(1));
+            case 'Each': return function(g) { return f.forEach(function(y) { return _A(g, y); }); };
             case 'Map': return function(g) { return f.map(function(y) { return _A(g, y); }); };
             case 'Filter': return function(g) { return f.filter(function(y) { return _B(_A(g, y)); }); };
+            case 'All': return function(g) { return f.every(function(y) { return _B(_A(g, y)); }); };
+            case 'Any': return function(g) { return f.some(function(y) { return _B(_A(g, y)); }); };
+            case 'Find': return function(g) { return _O(f.find(function(y) { return _B(_A(g, y)); })); };
+            case 'FindIndex': return function(g) { return _O(f.findIndex(function(y) { return _B(_A(g, y)); })); };
+            case 'Join': return function(s) { return f.join(s); };
+            case 'Slice': return function(a) { return function(b) { return f.slice(a, b); } };
+            case 'Fold': return function(g) { return function(x) { return f.reduce(g, x); } };
+            case 'FoldRight': return function(g) { return function(x) { return f.reduceRight(g, x); } };
             case '+': return function(v) { return f.concat(v); };
             default: throw "Unexpected argument: " + x;
         }
@@ -63,6 +71,11 @@ function _A(f, x) {
 // For using truth values with JavaScript logical operators
 function _B(x) {
     return x === true || x === 'True';
+}
+
+// For converting undefined/not undefined to {_ None} and {_ Some x}
+function _O(x) {
+    return x === void 0 ? function(g) { return _A(g, 'None'); } : function(g) { return _A(_A(g, 'Some'), x); };
 }
 
 // Global variables can be exposed to funk simply by giving them a "_" suffix:
